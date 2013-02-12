b73119bf0dd93246587845ad8d28392c1fd690a0
Sean Handley <sean.handley@gmail.com>
2013-02-12 11:50:00 +0000

    Umm...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seanhandley::Application.config.secret_token = '6f8c67a03e38200d326d2d63121cfcc91352c1f8d5b02d2379950198d4d5b7b70e44c0da25df7090443a34dc86c283906f0cb40df577add8d4a2f450a57d409f'
